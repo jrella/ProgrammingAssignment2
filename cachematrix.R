0454ff8630007bf363988cc0ac2b1f75f99c3596
jrella <jrella@digitalriver.com>
2015-11-21 14:44:26 -0300

    matrix solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+	  ## This function creates a special "matrix" object that can cache its inverse.
+  	  ##
+  	  ## Args:
+  	  ##   x: a square invertible matrix.
+  	  ##
+  	  ## Returns:
+  	  ##   a special "matrix" object that can cache its inverse.        
+        inverseValue = NULL
+        set = function(original) {
+                # use `<<-` to assign a value to an object in an environment different from the current environment. 
+                x <<- original
+                inverseValue <<- NULL
+        }
+        get = function() x
+
+        setinverse = function(inverse) inverseValue <<- inverse 
+
+        getinverse = function() inverseValue
+
+        list(set=set, get=get, setinverse = setinverse , getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##  If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	  ##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+        ##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+  	  ##
+  	  ## Args:
+  	  ##   x: output of makeCacheMatrix().
+  	  ##
+  	  ## Returns:
+  	  ##  inverse of the original matrix input to makeCacheMatrix(). 
+        
+        inverseValue = x$getinverse()
+        
+        # I check if the inverse has already been calculated
+        if (!is.null(inverseValue)){
+                message("getting cached data")
+                return(inverseValue)
+        }
+        
+        # I calculate the inverse value if it hasn't been calculated 
+        inverseValue = solve(x$get(), ...)
+        
+        # sets the value of the inverse in the cache via the setinv function.
+        x$setinverse(inverseValue)
+        
+        return(inverseValue)        
 }
